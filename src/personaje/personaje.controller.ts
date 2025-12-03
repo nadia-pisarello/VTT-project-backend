bfe30ff94d524e41d4800d20454025d2afd77622
Nadia Pisarello <pisarello.nads@gmail.com>
2025-12-02 21:54:36 -0300

    Agrega controlador de personaje

--- src/personaje/personaje.controller.ts
+++ src/personaje/personaje.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from '@nestjs/common';
+import { PersonajeService } from './personaje.service';
+import { CrearPersonajeDto } from './dto/crear-personaje.dto';
+import { UpdatePersonajeDto } from './dto/update-personaje.dto';
 
 @Controller('personaje')
-export class PersonajeController {}
+export class PersonajeController {
+    constructor(private readonly personajeServ: PersonajeService) { }
+
+    @Post(':usuarioId')
+    create(@Param('usuarioId', ParseIntPipe) usuarioId: number,
+        @Body() dto: CrearPersonajeDto) {
+        return this.personajeServ.createPersonaje(dto, usuarioId);
+    }
+
+    @Get('/usuario/:usuarioId')
+    findAll(@Param('usuarioId', ParseIntPipe) usuarioId: number) {
+        return this.personajeServ.getAllPersonajesUsuario(usuarioId);
+    }
+
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.personajeServ.getPersonajeById(id);
+    }
+
+    @Patch(':id/usuario/:usuarioId')
+    update(
+        @Param('id', ParseIntPipe) id: number,
+        @Param('usuarioId', ParseIntPipe) usuarioId: number,
+        @Body() dto: UpdatePersonajeDto) {
+        return this.personajeServ.updatePersonaje(id, dto, usuarioId);
+    }
+
+    @Delete(':id/usuario/:usuarioId')
+    remove(
+        @Param('id', ParseIntPipe) id: number,
+        @Param('usuarioId', ParseIntPipe) usuarioId: number) {
+        return this.personajeServ.deletePersonaje(id, usuarioId);
+    }
+
+}
